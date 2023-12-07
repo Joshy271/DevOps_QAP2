5cc3654b184915c67c0874b907bdc798ab53ea6b
Joshy <117656288+Joshy271@users.noreply.github.com>
2023-12-06 22:25:35 -0330

    database connected and table is created on startup

--- src/main/java/com/keyin/aircraft/aircraft.java
+++ src/main/java/com/keyin/aircraft/aircraft.java
@@ -2,7 +2,12 @@ package com.keyin.aircraft;
 
 import javax.persistence.*;
 import java.util.List;
+
+@Entity
 public class aircraft {
+    @Id
+    @SequenceGenerator(name = "aircraft_sequence", sequenceName = "aircraft_sequence", allocationSize = 1, initialValue=1)
+    @GeneratedValue(generator = "aircraft_sequence")
     private long id;
     private String name;
     private String code;
